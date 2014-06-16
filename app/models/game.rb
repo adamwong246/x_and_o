324392c70619c486be9fefbe4563191f87c51fbb
Adam Wong <adamwong246@gmail.com>
2014-06-15 20:43:08 -0700

    specs for testing the game state

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,38 @@ class Game < ActiveRecord::Base
 
   has_one :x_player, :foreign_key => 'x_user_id', :class_name => "User"
   has_one :o_player, :foreign_key => 'o_user_id', :class_name => "User"
+
+  def win_cat_or_else
+    array = board.split("").each_with_index.map{|x, i|
+      [i/3][i%3] = x
+    }
+
+    [:x, :o].each do |p|
+      if ([array[0][0], array[0][1], array[0][2], p.to_s].uniq.length == 1) ||
+         ([array[1][0], array[1][1], array[1][2], p.to_s].uniq.length == 1) ||
+         ([array[2][0], array[2][1], array[2][2], p.to_s].uniq.length == 1) ||
+         ([array[1][0], array[1][0], array[2][0], p.to_s].uniq.length == 1) ||
+         ([array[1][1], array[1][1], array[2][1], p.to_s].uniq.length == 1) ||
+         ([array[1][2], array[1][2], array[2][2], p.to_s].uniq.length == 1) ||
+         ([array[0][0], array[1][1], array[2][2], p.to_s].uniq.length == 1) ||
+         ([array[0][2], array[1][1], array[2][0], p.to_s].uniq.length == 1) 
+
+         return {win: p}
+      end
+    end
+
+    if board.delete(' ').length >= 9
+      return :cat
+    else
+      if board.count("x") > board.count("o")
+        return {continue: :o}
+      else
+        return {continue: :x}
+      end
+    end
+
+    raise 'wtf?'
+
+  end
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Game, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Game do
+  describe "win_cat_or_else" do
+
+    it "should know a win" do
+      game = Game.new(board: 'xxxoxoxox')
+      game.win_cat_or_else.should eq({win: :x})
+    end
+
+    it "should know a stalemate" do
+      game = Game.new(board: 'xxoooxxox')
+      game.win_cat_or_else.should eq( :cat )
+    end
+
+    it "should know to continue" do
+      game = Game.new(board: 'x        ')
+      game.win_cat_or_else.should eq({continue: :o})
+    end
+
+    it "should know to continue" do
+      game = Game.new(board: 'xo       ')
+      game.win_cat_or_else.should eq({continue: :x})
+    end
+
+  end
 end
