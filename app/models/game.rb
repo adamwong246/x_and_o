7bc62c77606f2d407fa3390053dc70c843d51632
Adam Wong <adamwong246@gmail.com>
2014-06-15 19:13:56 -0700

    setup model accessor attributes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
-  attr_accessible :board, :o_user_id, :x_user_id
+  attr_accessible :board, :o_user_id, :x_user_id, :x_player
+
+  has_one :x_player, :foreign_key => 'x_user_id', :class_name => "User"
+  has_one :o_player, :foreign_key => 'o_user_id', :class_name => "User"
 end
