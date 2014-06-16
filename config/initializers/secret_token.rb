5abfbb23a2cdbe2a978bf89cb4b89f25fc18048c
Adam Wong <adamwong246@gmail.com>
2014-06-15 18:35:34 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XAndO::Application.config.secret_token = '6b0ad10cc8cc964d602fe2e015b41b2bb2bc448918ce4ce9f88b27d12545698a4cfac891ea34c53fd6eedbb8bf5d48e6e197d6b2a2fbc7188180b1743e07ddfc'
